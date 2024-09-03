48250b02416414deff7218a0f786ff65c2c906d7
N4din3 <nadine.schroeder03@gmx.de>
2024-09-03 08:54:59 +0200

    Zweite Version
    -Befehle hinzugefügt, Groß- und Kleinschreibung dadurch egal.

--- Umrechnung Meter in Feet.py
+++ Umrechnung Meter in Feet.py
@@ -6,20 +6,20 @@ eingabeJaNein = 1
 while eingabeJaNein == 1:
 
 # Umrechnung von Meter in Feet
-    wahl = input("Welcher Wert steht dir zur verfügung? Meter oder Feet?: ")
-    if wahl == "Meter":
+    wahl = input("Welcher Wert steht dir zur verfügung? Meter oder Feet?: ").lower()
+    if wahl == "meter":
         inputMeter = float(input("Gib den Wert in Meter an: "))
         outputFeet = inputMeter * umrechnungsfaktorMeter_Feet
         print(inputMeter,"Meter sind",outputFeet,"Feet")
         
 # Umrechnung von Feet in Meter
-    elif wahl == "Feet":
+    elif wahl == "feet":
         inputFeet = float(input("Gib den Wert in Feet an: "))
         outputMeter = inputFeet / umrechnungsfaktorMeter_Feet
         print(inputFeet,"Feet sind",outputMeter,"Meter")
 
 # Sprung zum Anfang (while-Schleife)
-    neueEingabe = input("Willst du eine neue Umrechnung machen? Ja oder Nein? ")
+    neueEingabe = input("Willst du eine neue Umrechnung machen? Ja oder Nein? ").lower()
     if neueEingabe == "Ja":
         eingabeJaNein = 1
     elif neueEingabe == "Nein":
